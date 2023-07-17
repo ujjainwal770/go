848762c6a3dd10a1491501606de68cee5db4a85d
Joker <50817370+ujjainwal770@users.noreply.github.com>
2023-07-17 18:01:12 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,12 +1,9 @@
-provider "azurerm" {
-  version = ">= 2.0.0"
-  features {}
-}
-
 terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
+      version = ">= 2.0.0"
     }
   }
 }
+
