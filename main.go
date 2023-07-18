14604e867df2bd0bcfa7634e78993fe291842fa3
Joker <50817370+ujjainwal770@users.noreply.github.com>
2023-07-18 13:11:41 +0530

    Update main.go

--- main.go
+++ main.go
@@ -13,16 +13,36 @@ func main() {
 	}
 
 	cloudProvider := os.Args[1]
-	err := os.Chdir(cloudProvider)
-	if err != nil {
-		log.Fatalf("Failed to change directory to %s: %v", cloudProvider, err)
-	}
 
-	// Run the Go program in the selected cloud provider folder
-	cmd := exec.Command("go", "run", "main.go")
+	switch cloudProvider {
+	case "aws":
+		// Set up AWS-specific configurations and execute Terraform commands
+		fmt.Println("Configuring AWS provider...")
+		runTerraformCommand("init", "aws")
+		runTerraformCommand("plan", "aws")
+		runTerraformCommand("apply", "aws")
+	case "azure":
+		// Set up Azure-specific configurations and execute Terraform commands
+		fmt.Println("Configuring Azure provider...")
+		runTerraformCommand("init", "azure")
+		runTerraformCommand("plan", "azure")
+		runTerraformCommand("apply", "azure")
+	case "gcp":
+		// Set up Google Cloud-specific configurations and execute Terraform commands
+		fmt.Println("Configuring Google Cloud provider...")
+		runTerraformCommand("init", "gcp")
+		runTerraformCommand("plan", "gcp")
+		runTerraformCommand("apply", "gcp")
+	default:
+		log.Fatal("Invalid cloud provider selected")
+	}
+}
+
+func runTerraformCommand(command string, provider string) {
+	cmd := exec.Command("terraform", command, "-var-file=terraform.tfvars")
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
-	err = cmd.Run()
+	err := cmd.Run()
 	if err != nil {
 		log.Fatal(err)
 	}
